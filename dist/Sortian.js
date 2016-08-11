fbe642a1bff562b7a310af8946908e2abbca3dcb
Phantr4x <phantr4x@gmail.com>
2016-08-11 21:27:44 +0800

    - add heapSort

--- dist/Sortian.js
+++ dist/Sortian.js
@@ -31,23 +31,26 @@ Sortian.bubble = function(array) {
 };
 
 Sortian.quickSort = function(array) {
+  // 交换元素位置
   function swap(array, i, k) {
     var temp = array[i];
     array[i] = array[k];
     array[k] = temp;
   }
+  // 数组分区，左小右大
   function partition(array, left, right) {
     var storeIndex = left;
-    var pivot = array[right];
+    var pivot = array[right]; // 直接选最右边的元素为基准元素
     for (var i = left; i < right; i++) {
       if (array[i] < pivot) {
         swap(array, storeIndex, i);
-        storeIndex++;
+        storeIndex++; // 交换位置后，storeIndex 自增 1，代表下一个可能要交换的位置
       }
     }
-    swap(array, right, storeIndex);
+    swap(array, right, storeIndex); // 将基准元素放置到最后的正确位置上
     return storeIndex;
   }
+
   function sort(array, left, right) {
     if (left > right) {
       return;
@@ -56,6 +59,7 @@ Sortian.quickSort = function(array) {
     sort(array, left, storeIndex - 1);
     sort(array, storeIndex + 1, right);
   }
+
   sort(array, 0, array.length - 1);
   return array;
 };
@@ -76,6 +80,8 @@ Sortian.selectionSort = function(array) {
         minValue = array[minIndex];
       }
     }
+
+    // 交换位置
     temp = array[i];
     array[i] = minValue;
     array[minIndex] = temp;
@@ -84,8 +90,64 @@ Sortian.selectionSort = function(array) {
 };
 
 
+Sortian.heapSort = function(array) {
+  function swap(array, i, j) {
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+
+  function maxHeapify(array, index, heapSize) {
+    var iMax,
+      iLeft,
+      iRight;
+    while (true) {
+      iMax = index;
+      iLeft = 2 * index + 1;
+      iRight = 2 * (index + 1);
+
+      if (iLeft < heapSize && array[index] < array[iLeft]) {
+        iMax = iLeft;
+      }
+
+      if (iRight < heapSize && array[iMax] < array[iRight]) {
+        iMax = iRight;
+      }
+
+      if (iMax != index) {
+        swap(array, iMax, index);
+        index = iMax;
+      } else {
+        break;
+      }
+    }
+  }
+
+  function buildMaxHeap(array) {
+    var i,
+      iParent = Math.floor(array.length / 2) - 1;
+
+    for (i = iParent; i >= 0; i--) {
+      maxHeapify(array, i, array.length);
+    }
+  }
+
+  function sort(array) {
+    buildMaxHeap(array);
+
+    for (var i = array.length - 1; i > 0; i--) {
+      swap(array, 0, i);
+      maxHeapify(array, 0, i);
+    }
+    return array;
+  }
+
+  return sort(array);
+};
+
+
 // Version.
-Sortian.VERSION = '0.0.0';
+Sortian.VERSION = '0.2.0';
 
 
 // Export to the root, which is probably `window`.
